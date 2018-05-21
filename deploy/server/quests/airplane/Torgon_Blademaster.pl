3e35b6439eec48055d57e20e1c42c2c0cc8101cf
jon <jon@LAPTOP-3J1QRI6S>
2018-05-21 01:42:06 -0400

    Added quest script for Tazgar. Modified the other quest scripts for NPCs that accept bottles of experience. Previously, handing more than one stack of experience bottles would return all of them to the player, but they would get credit. Also, by adding the script to Tazgar, this prevents the player from killing him to get the potions back.

--- deploy/server/quests/airplane/Torgon_Blademaster.pl
+++ deploy/server/quests/airplane/Torgon_Blademaster.pl
@@ -112,8 +112,21 @@ sub EVENT_ITEM {
     return;
   }
 
-  if (plugin::check_handin(\%itemcount, 100001 => 1)) {
-    quest::say("This shows your experience.");
+  #Need to check if 1,2,3 or 4 stacks of experience bottles are turned in, otherwise player gets credit and items are returned.
+  if(plugin::check_handin(\%itemcount, 100001 => 1)) {
+	quest::say("Thank you for your potion of experience, $name.");
+    return;
+  }
+    if(plugin::check_handin(\%itemcount, 100001 => 2)) {
+	quest::say("Thank you for your potion of experience, $name.");
+    return;
+  }
+    if(plugin::check_handin(\%itemcount, 100001 => 3)) {
+	quest::say("Thank you for your potion of experience, $name.");
+    return;
+  }
+    if(plugin::check_handin(\%itemcount, 100001 => 4)) {
+	quest::say("Thank you for your potion of experience, $name.");
     return;
   }
 
